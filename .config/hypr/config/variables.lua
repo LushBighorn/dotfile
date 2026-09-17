4e9b1511715add447f08f53b88e3f01e6e7f266b
LushBighorn <j.bahati05@gmail.com>
2026-09-17 01:08:05 -0400

    add hypr files

---
+++ .config/hypr/config/variables.lua
@@ -0,0 +1,17 @@
+-- Hyprland default apps
+
+TERMINAL     = "kitty"
+FILE_MANAGER = "dolphin"
+BROWSER      = "firefox"
+EDITOR       = "gnome-text-editor --new-window"
+CALCULATOR   = "gnome-calculator"
+MUSIC = "spotify-launcher"
+
+-- Monitors
+MONITOR1 = "DP-1"
+MONITOR2 = ""
+MONITOR3 = ""
+PRIMARY_MONITOR = MONITOR1
+
+-- Workspaces
+NUM_WPM = 4 -- Number of workspaces per monitor (Max 10)
